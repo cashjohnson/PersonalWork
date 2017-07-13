6a7f7c45b33a10b6ce6ba06e1cb21f0ea0b827e8
Cash.Johnson <type2cash@gmail.com>
2017-07-12 22:27:12 -0600

    Changed LEDDisplayDemo to take a string input; Created tempSensor.py

--- relayFiles/LEDDisplayDemo/LEDDisplayDemo.ino
+++ relayFiles/LEDDisplayDemo/LEDDisplayDemo.ino
@@ -1,32 +1,32 @@
 /*
- 6-13-2011
- Spark Fun Electronics 2011
- Nathan Seidle
- 
- This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
- 4 digit 7 segment display:
- http://www.sparkfun.com/products/9483
- Datasheet: 
- http://www.sparkfun.com/datasheets/Components/LED/7-Segment/YSD-439AR6B-35.pdf
+  6-13-2011
+  Spark Fun Electronics 2011
+  Nathan Seidle
 
- This is an example of how to drive a 7 segment LED display from an ATmega without the use of current limiting resistors.
- This technique is very common but requires some knowledge of electronics - you do run the risk of dumping too 
- much current through the segments and burning out parts of the display. If you use the stock code you should be ok, but 
- be careful editing the brightness values.
- 
- This code should work with all colors (red, blue, yellow, green) but the brightness will vary from one color to the next
- because the forward voltage drop of each color is different. This code was written and calibrated for the red color.
+  This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
- This code will work with most Arduinos but you may want to re-route some of the pins.
+  4 digit 7 segment display:
+  http://www.sparkfun.com/products/9483
+  Datasheet:
+  http://www.sparkfun.com/datasheets/Components/LED/7-Segment/YSD-439AR6B-35.pdf
 
- 7 segments
- 4 digits
- 1 colon
- =
- 12 pins required for full control 
- 
- */
+  This is an example of how to drive a 7 segment LED display from an ATmega without the use of current limiting resistors.
+  This technique is very common but requires some knowledge of electronics - you do run the risk of dumping too
+  much current through the segments and burning out parts of the display. If you use the stock code you should be ok, but
+  be careful editing the brightness values.
+
+  This code should work with all colors (red, blue, yellow, green) but the brightness will vary from one color to the next
+  because the forward voltage drop of each color is different. This code was written and calibrated for the red color.
+
+  This code will work with most Arduinos but you may want to re-route some of the pins.
+
+  7 segments
+  4 digits
+  1 colon
+  =
+  12 pins required for full control
+
+*/
 
 int digit1 = 2; //PWM Display pin 1
 int digit2 = 3; //PWM Display pin 2
@@ -43,9 +43,12 @@ int segE = 10; //Display pin 5
 int segF = 11; //Display pin 11
 int segG = 12; //Display pin 15
 
+int DP = A0; //Display pin 7
+
 #include <Time.h>
 
-void setup() {                
+void setup() {
+  Serial.begin(9600);
   pinMode(segA, OUTPUT);
   pinMode(segB, OUTPUT);
   pinMode(segC, OUTPUT);
@@ -53,26 +56,18 @@ void setup() {
   pinMode(segE, OUTPUT);
   pinMode(segF, OUTPUT);
   pinMode(segG, OUTPUT);
+  pinMode(DP, OUTPUT);
 
   pinMode(digit1, OUTPUT);
   pinMode(digit2, OUTPUT);
   pinMode(digit3, OUTPUT);
   pinMode(digit4, OUTPUT);
-  
-  pinMode(13, OUTPUT);
 }
 
+float thing = 98.76;
+
 void loop() {
-  
-  //long startTime = millis();
-
-  displayNumber(millis()/1000);
-
-  //while( (millis() - startTime) < 2000) {
-  
-//  displayNumber(hour()+minute());
-  //}
-  //delay(1000);  
+  displayString((String)thing);
 }
 
 //Given a number, we display 10:22
@@ -94,49 +89,77 @@ void loop() {
 //5 dim but readable (0.31mA)
 //1 dim but readable in dark (0.28mA)
 
-void displayNumber(int toDisplay) {
+void displayString(String toDisplay) {
 #define DISPLAY_BRIGHTNESS  1
-
 #define DIGIT_ON  HIGH
 #define DIGIT_OFF  LOW
-
   long beginTime = millis();
-
-  for(int digit = 4 ; digit > 0 ; digit--) {
-
-    //Turn on a digit for a short amount of time
-    switch(digit) {
-    case 1:
-      digitalWrite(digit1, DIGIT_ON);
-      break;
-    case 2:
-      digitalWrite(digit2, DIGIT_ON);
-      break;
-    case 3:
-      digitalWrite(digit3, DIGIT_ON);
-      break;
-    case 4:
-      digitalWrite(digit4, DIGIT_ON);
-      break;
+  for (int iter = 0 ; iter < 5 ; iter++) {
+    switch (iter) {
+      case 0:
+        digitalWrite(digit1, DIGIT_ON);
+        break;
+      case 1:
+        digitalWrite(digit2, DIGIT_ON);
+        break;
+      case 2:
+        digitalWrite(digit2, DIGIT_ON);
+        break;
+      case 3:
+        digitalWrite(digit3, DIGIT_ON);
+        break;
+      case 4:
+        digitalWrite(digit4, DIGIT_ON);
+        break;
     }
+    lightNumber(toDisplay[iter]);
+    delayMicroseconds(DISPLAY_BRIGHTNESS); //Display this digit for a fraction of a second (between 1us and 5000us, 500 is pretty good)
+    //Turn off all segments
+    lightNumber(10);
+    //Turn off all digits
+    digitalWrite(digit1, DIGIT_OFF);
+    digitalWrite(digit2, DIGIT_OFF);
+    digitalWrite(digit3, DIGIT_OFF);
+    digitalWrite(digit4, DIGIT_OFF);
+    Serial.println(toDisplay[iter]);
+  }
+  while ( (millis() - beginTime) < 10) ; //Wait for 20ms to pass before we paint the display again
+}
 
+void displayNumber(int toDisplay) {
+#define DISPLAY_BRIGHTNESS  1
+#define DIGIT_ON  HIGH
+#define DIGIT_OFF  LOW
+  long beginTime = millis();
+  for (int digit = 4 ; digit > 0 ; digit--) {
+    //Turn on a digit for a short amount of time
+    switch (digit) {
+      case 1:
+        digitalWrite(digit1, DIGIT_ON);
+        break;
+      case 2:
+        digitalWrite(digit2, DIGIT_ON);
+        break;
+      case 3:
+        digitalWrite(digit3, DIGIT_ON);
+        break;
+      case 4:
+        digitalWrite(digit4, DIGIT_ON);
+        break;
+    }
     //Turn on the right segments for this digit
     lightNumber(toDisplay % 10);
     toDisplay /= 10;
-
     delayMicroseconds(DISPLAY_BRIGHTNESS); //Display this digit for a fraction of a second (between 1us and 5000us, 500 is pretty good)
-
     //Turn off all segments
-    lightNumber(10); 
-
+    lightNumber(10);
     //Turn off all digits
     digitalWrite(digit1, DIGIT_OFF);
     digitalWrite(digit2, DIGIT_OFF);
     digitalWrite(digit3, DIGIT_OFF);
     digitalWrite(digit4, DIGIT_OFF);
   }
-
-  while( (millis() - beginTime) < 10) ; //Wait for 20ms to pass before we paint the display again
+  while ( (millis() - beginTime) < 10) ; //Wait for 20ms to pass before we paint the display again
 }
 
 //Given a number, turns on those segments
@@ -146,117 +169,138 @@ void lightNumber(int numberToDisplay) {
 #define SEGMENT_ON  LOW
 #define SEGMENT_OFF HIGH
 
-  switch (numberToDisplay){
+  switch (numberToDisplay) {
 
-  case 0:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_ON);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_OFF);
-    break;
+    case '0':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_OFF);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 1:
-    digitalWrite(segA, SEGMENT_OFF);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_OFF);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_OFF);
-    digitalWrite(segG, SEGMENT_OFF);
-    break;
+    case '1':
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 2:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_OFF);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_ON);
-    digitalWrite(segF, SEGMENT_OFF);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '2':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_OFF);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 3:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_OFF);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '3':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 4:
-    digitalWrite(segA, SEGMENT_OFF);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_OFF);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '4':
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 5:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_OFF);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '5':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 6:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_OFF);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_ON);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '6':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 7:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_OFF);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_OFF);
-    digitalWrite(segG, SEGMENT_OFF);
-    break;
+    case '7':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 8:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_ON);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '8':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_ON);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 9:
-    digitalWrite(segA, SEGMENT_ON);
-    digitalWrite(segB, SEGMENT_ON);
-    digitalWrite(segC, SEGMENT_ON);
-    digitalWrite(segD, SEGMENT_ON);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_ON);
-    digitalWrite(segG, SEGMENT_ON);
-    break;
+    case '9':
+      digitalWrite(segA, SEGMENT_ON);
+      digitalWrite(segB, SEGMENT_ON);
+      digitalWrite(segC, SEGMENT_ON);
+      digitalWrite(segD, SEGMENT_ON);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_ON);
+      digitalWrite(segG, SEGMENT_ON);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
 
-  case 10:
-    digitalWrite(segA, SEGMENT_OFF);
-    digitalWrite(segB, SEGMENT_OFF);
-    digitalWrite(segC, SEGMENT_OFF);
-    digitalWrite(segD, SEGMENT_OFF);
-    digitalWrite(segE, SEGMENT_OFF);
-    digitalWrite(segF, SEGMENT_OFF);
-    digitalWrite(segG, SEGMENT_OFF);
-    break;
+    case 10:
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_OFF);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+      digitalWrite(DP, SEGMENT_OFF);
+      break;
+
+    case '.':
+      digitalWrite(segA, SEGMENT_OFF);
+      digitalWrite(segB, SEGMENT_OFF);
+      digitalWrite(segC, SEGMENT_OFF);
+      digitalWrite(segD, SEGMENT_OFF);
+      digitalWrite(segE, SEGMENT_OFF);
+      digitalWrite(segF, SEGMENT_OFF);
+      digitalWrite(segG, SEGMENT_OFF);
+      digitalWrite(DP, SEGMENT_ON);
   }
 }
 
