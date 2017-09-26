dfe683c7f482c6fdcccf989c4e97a8bdb1b24211
cash <type2cash@gmail.com>
2017-09-25 18:21:37 -0600

    Starting dev on frontend

---
+++ relayFiles/html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Starting Place</title>
+</head>
+<body>
+	<h5>First</h5>
+	<p>Something to build on</p>
+
+<?php
+	echo "Hello from php";
+?>
+
+</body>
+</html>
