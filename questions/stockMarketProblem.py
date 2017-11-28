25e92d9135c3ad9cf2b5e37fd2b8b19f048aacbb
Cash.Johnson <type2cash@gmail.com>
2017-11-28 14:35:47 -0700

    Adding Stock Market Example problem

---
+++ questions/stockMarketProblem.py
@@ -0,0 +1,9 @@
+textInput = "65 12 40 12 56 12 45 78 69 51 68 35 78 18 98 68 48 55 68 78 99 48 78 25 68"
+
+values = textInput.split()
+
+bestBuyIndex = 1
+bestSellIndex = 2
+
+for i in range(1,len(values)):
+    for j in range
