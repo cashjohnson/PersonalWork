d8ed375a6c9fd1f59e4aff0a6628710034b67f19
cash <type2cash@gmail.com>
2017-12-19 15:37:25 -0700

    Solving a practice problem I found on the internet

--- questions/stockMarketProblem.py
+++ questions/stockMarketProblem.py
@@ -1,9 +1,19 @@
-textInput = "65 12 40 12 56 12 45 78 69 51 68 35 78 18 98 68 48 55 68 78 99 48 78 25 68"
+# This is a problem I found on the internet which caught my eye. If we recoreded the price of a stock
+# at regular intervals and converted it to a string input (which i made up in textInput) where would be the 
+# most money I could make based on the buy and sell price. 
 
-values = textInput.split()
+textInput = "65 12 40 12 56 12 45 78 69 51 68 35 78 18 98 68 48 55 68 78 99 48 78 25 68"#random string of inputs 
+values = textInput.split()#turning the string input into an array for ease of use
 
-bestBuyIndex = 1
+bestBuyIndex = 1#setting an arbitrary starting place at the beginning
 bestSellIndex = 2
+bestPrice = int(values[2]) - int(values[1])
 
-for i in range(1,len(values)):
-    for j in range
+for i in range(1,len(values)):#iterate through possible buy locations
+    for j in range(2,len(values)):#iterate through possible sell locations
+    	if (int(values[j])-int(values[i]) > bestPrice):#switching the indexes to reflect the newest best prices
+    		bestBuyIndex = int(values[i])
+    		bestSellIndex = int(values[j])
+    		bestPrice = int(values[j]) - int(values[i])
+
+print "The most money you could make is %d buying at %d and selling at %d"%(bestPrice, bestBuyIndex, bestSellIndex)
