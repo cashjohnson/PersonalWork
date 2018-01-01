33a103cfaa944b2b3739430cdf04a97e402ad5e2
cash <type2cash@gmail.com>
2018-01-01 06:19:10 -0700

    Changing the implementation to object based as opposed to an array

--- api/runePriceChecker.py
+++ api/runePriceChecker.py
@@ -17,18 +17,71 @@ urlBodyRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.jso
 urlEarthRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=e&page=1'
 urlFireRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=f&page=1'
 urlWaterRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=w&page=1'
+urlMindRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=m&page=1'
+urlCosmicRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=c&page=1'
+urlChaosRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=c&page=1'
+urlNatureRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=n&page=1'
+urlLawRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=l&page=1'
+urlDeathRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=d&page=1'
+urlAstralRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=a&page=1'
+urlBloodRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=b&page=1'
+urlSoulRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=s&page=1'
 
-urlArray = [urlAirRune,urlBodyRune,urlEarthRune,urlFireRune,urlWaterRune]
-apiIndex = [0,2,0,1,1]
-prices = []
+class Runes:
+    apiUrl = ""
+    apiIndex = ""
+    currentPrice = 0
+    name = ""
 
-for i in range(0,len(urlArray)):
-    response = urllib.urlopen(urlArray[i])
-    data = json.loads(response.read())
-    itemName = data["items"][apiIndex[i]]["name"]
-    itemPrice = data["items"][apiIndex[i]]["current"]["price"]
-    print('%s current GE price is %s gp' % (itemName, itemPrice))
-    prices[i] = data["items"][apiIndex[i]]["current"]["price"]
+    def __init__(self, url, apiIndex):
+        self.apiUrl = url
+        self.apiIndex = apiIndex
+        response = urllib.urlopen(self.apiUrl)
+        data = json.loads(response.read())
+        self.name = data["items"][self.apiIndex]["name"]
+        self.currentPrice = data["items"][self.apiIndex]["current"]["price"]
+
+    def extractValues(self):
+        response = urllib.urlopen(self.apiUrl)
+        data = json.loads(response.read())
+        self.name = data["items"][self.apiIndex]["name"]
+        self.currentPrice = data["items"][self.apiIndex]["current"]["price"]  
+
+    def showValues(self):
+        print('This is %s' % self.name)
+        print('The apiIndex = %s' % self.apiIndex)
+        print('The current price = %d' % self.currentPrice)
+        print('')
+
+airRune = Runes(urlAirRune, 0)
+bodyRune = Runes(urlBodyRune, 2)
+earthRune = Runes(urlEarthRune, 0)
+fireRune = Runes(urlFireRune, 1)
+waterRune = Runes(urlWaterRune, 1)
+mindRune = Runes(urlMindRune, 1)
+cosmicRune = Runes(urlCosmicRune, 3)
+chaosRune = Runes(urlChaosRune, 2)
+natureRune = Runes(urlNatureRune, 0)
+lawRune = Runes(urlLawRune, 2)
+deathRune = Runes(urlDeathRune, 2)
+astralRune = Runes(urlAstralRune, 3)
+bloodRune = Runes(urlBloodRune, 1)
+soulRune = Runes(urlSoulRune, 3)
+
+airRune.showValues()
+bodyRune.showValues()
+earthRune.showValues()
+fireRune.showValues()
+waterRune.showValues()
+mindRune.showValues()
+cosmicRune.showValues()
+chaosRune.showValues()
+natureRune.showValues()
+lawRune.showValues()
+deathRune.showValues()
+astralRune.showValues()
+bloodRune.showValues()
+soulRune.showValues()
 
 # Carwen Essencebinder Magical Runes Shop
 print("Teleport to the Burthrope lodestone, run east to Carwen Essencebinder Magical Runes Shop")
--- api/test.py
+++ api/test.py
@@ -3,21 +3,34 @@
 import json
 import urllib
 
-urlAirRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=a&page=1'
-urlBodyRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=b&page=1'
-urlEarthRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=e&page=1'
-urlFireRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=f&page=1'
-urlWaterRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=w&page=1'
-urlChaosRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=c&page=1'
+url = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=s&page=1'
+
+for i in range(0,5):
+	try:
+		response = urllib.urlopen(url)
+		data = json.loads(response.read())
+		itemName = data["items"][i]["name"]
+		itemPrice = data["items"][i]["current"]["price"]
+		print('i:%d %s current GE price is %s gp' % (i, itemName, itemPrice))
+	except:
+		pass
 
 
-urlArray = [urlAirRune,urlBodyRune,urlChaosRune,urlEarthRune,urlFireRune,urlWaterRune]
-apiIndex = [0,2,2,0,1,1]
+# urlAirRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=a&page=1'
+# urlBodyRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=b&page=1'
+# urlEarthRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=e&page=1'
+# urlFireRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=f&page=1'
+# urlWaterRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=w&page=1'
+# urlChaosRune = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=32&alpha=c&page=1'
 
-for i in range(0,len(urlArray)):
-    response = urllib.urlopen(urlArray[i])
-    data = json.loads(response.read())
-    itemName = data["items"][apiIndex[i]]["name"]
-    itemPrice = data["items"][apiIndex[i]]["current"]["price"]
-    print('%s current GE price is %s gp' % (itemName, itemPrice))
-    # prices[i] = data["items"][apiIndex[i]]["current"]["price"]
+
+# urlArray = [urlAirRune,urlBodyRune,urlChaosRune,urlEarthRune,urlFireRune,urlWaterRune]
+# apiIndex = [0,2,2,0,1,1]
+
+# for i in range(0,len(urlArray)):
+#     response = urllib.urlopen(urlArray[i])
+#     data = json.loads(response.read())
+#     itemName = data["items"][apiIndex[i]]["name"]
+#     itemPrice = data["items"][apiIndex[i]]["current"]["price"]
+# print('%s current GE price is %s gp' % (itemName, itemPrice))
+# prices[i] = data["items"][apiIndex[i]]["current"]["price"]
