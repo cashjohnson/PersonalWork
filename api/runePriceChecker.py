5cf813f3ed36938ff76e7bcde45763cc462e5ad8
Cash.Johnson <type2cash@gmail.com>
2018-01-20 16:04:43 -0700

    Finished the inital verison. Still reqiures ingame checking. Would like to add an expected gold output

--- api/runePriceChecker.py
+++ api/runePriceChecker.py
@@ -9,6 +9,15 @@
 # a tool that would tell me where/which runes to buy from vendors and sell on the Grand Exchange (a central 
 # auction house in the game).
 
+# items
+# print(data)
+# print(data["items"][0]["name"])
+# print()
+# print(data["items"][0]["current"]["price"])
+
+# detail
+# print(data["item"]["current"])
+
 import json
 import urllib
 
@@ -53,6 +62,31 @@ class Runes:
         print('The current price = %d' % self.currentPrice)
         print('')
 
+def standardPriceCheck():
+    print("")
+    if (fireRune.currentPrice > 17):
+        print("Buy Fire Runes")
+    if (waterRune.currentPrice > 17):
+        print("Buy Water Runes")
+    if (airRune.currentPrice > 17):
+        print("Buy Air Runes")
+    if (earthRune.currentPrice > 17):
+        print("Buy Earth Runes")
+    if (mindRune.currentPrice > 17):
+        print("Buy Mind Runes")
+    if (bodyRune.currentPrice > 16):
+        print("Buy Body Runes")
+    if (chaosRune.currentPrice > 140):
+        print("Buy Chaos Rune")
+    if (deathRune.currentPrice > 310):
+        print("Buy Death Rune")
+    print("")
+    raw_input("Press Enter to continue...")
+    print("")
+
+print("Downloading Values from Runescape API!")
+
+
 airRune = Runes(urlAirRune, 0)
 bodyRune = Runes(urlBodyRune, 2)
 earthRune = Runes(urlEarthRune, 0)
@@ -68,35 +102,84 @@ astralRune = Runes(urlAstralRune, 3)
 bloodRune = Runes(urlBloodRune, 1)
 soulRune = Runes(urlSoulRune, 3)
 
-airRune.showValues()
-bodyRune.showValues()
-earthRune.showValues()
-fireRune.showValues()
-waterRune.showValues()
-mindRune.showValues()
-cosmicRune.showValues()
-chaosRune.showValues()
-natureRune.showValues()
-lawRune.showValues()
-deathRune.showValues()
-astralRune.showValues()
-bloodRune.showValues()
-soulRune.showValues()
+# airRune.showValues()
+# bodyRune.showValues()
+# earthRune.showValues()
+# fireRune.showValues()
+# waterRune.showValues()
+# mindRune.showValues()
+# cosmicRune.showValues()
+# chaosRune.showValues()
+# natureRune.showValues()
+# lawRune.showValues()
+# deathRune.showValues()
+# astralRune.showValues()
+# bloodRune.showValues()
+# soulRune.showValues()
+print("")
 
-# Carwen Essencebinder Magical Runes Shop
+#1 Carwen Essencebinder Magical Runes Shop
 print("Teleport to the Burthrope lodestone, run east to Carwen Essencebinder Magical Runes Shop")
+standardPriceCheck()
 
+#2 Aubury's Rune Shop
+print("Teleport to the Varrock lodestone, run north-east to Aubury's Rune Shop")
+standardPriceCheck()
 
-#[GEAirRune,GEBodyRune,GEEarthRune,GEFireRune,GEWaterRune]
+#3 Ali's Discount Wares
+# print("Teleport to the Al Kharid lodestone, run north to Ali's Discount Wares")
+# print("Opening Ali's rune shop requires talking to him and selecting the appropriate dialogue options (\"I would like to have a look at your selection of runes.\" and \"Buy elemental runes.\")")
 
+#4 Betty's Magic Emporium
+print("Teleport to the Port Sarim lodestone, run north to Betty's Magic Emporium (located in the northernmost house in Port Sarim)")
+standardPriceCheck()
 
-# items
-# print(data)
-# print(data["items"][0]["name"])
-# print()
-# print(data["items"][0]["current"]["price"])
+#5 Void Knight Magic Store
+print("From Betty's shop, run south to the second-southernmost pier at the Port Sarim docks, and click on the Squire to travel to the Void Knights' Outpost. From your arrival spot in the outpost run south-west to the Void Knight Magic Store")
+standardPriceCheck()
 
+#6 Magic Guild Store - Runes and Staves
+print("Teleport to the Yanille lodestone, run east to the Magic Guild, go up the stairs to the second floor where Magic Guild Store - Runes and Staves is located")
+standardPriceCheck()
 
-# detail
-# print(data["item"]["current"])
+#7 Baba Yaga's Magic Shop
+print("Teleport to the Lunar Isle lodestone, click the walking house to the north to enter it and reach Baba Yaga who runs the Baba Yaga's Magic Shop")
+standardPriceCheck()
 
+#8 Ape Atoll
+print("Travel to Ape Atoll either by casting the Teleport to Ape Atoll spell or by other means, equip a greegree, and run to Tutab's Magical Market, located in the southern part of the marketplace")
+standardPriceCheck()
+
+#9 Edgeville
+print("Teleport to the Edgeville lodestone, bank all your items, and run north to the Mage of Zamorak's shop Battle Runes, located in level 5 Wilderness")
+standardPriceCheck()
+
+#10 Wilderness! D=
+print("Return to Edgeville and bank all your items. Make sure you have a knife in your tool belt or are carrying a slashing weapon. Head south to the ruined building in Edgeville and travel to level 51 Wilderness by pulling the Edgeville teleport lever. Exit the Deserted Keep you find yourself in by cutting through the cobweb blocking the way north, and head west to a small ruined building north of the Mage Arena. Cut through the two cobwebs blocking the way and pull the lever to be transported to the small cave where Lundail runs his Arena-side Rune Shop. This is a safe area that isn't considered Wilderness and also contains a bank, so you may purchase the runes from the shop in peace.")
+print("")
+if (fireRune.currentPrice > 17):
+    print("Buy Fire Runes")
+if (waterRune.currentPrice > 17):
+    print("Buy Water Runes")
+if (airRune.currentPrice > 17):
+    print("Buy Air Runes")
+if (earthRune.currentPrice > 17):
+    print("Buy Earth Runes")
+if (mindRune.currentPrice > 17):
+    print("Buy Mind Runes")
+if (bodyRune.currentPrice > 16):
+    print("Buy Body Runes")
+if (natureRune.currentPrice > 372):
+    print("Buy Nature Rune")
+if (chaosRune.currentPrice > 140):
+    print("Buy Chaos Rune")
+if (lawRune.currentPrice > 378):
+    print("Buy Law Rune")
+if (cosmicRune.currentPrice > 232):
+    print("Buy Cosmic Rune")
+if (deathRune.currentPrice > 310):
+    print("Buy Death Rune")
+print("")
+raw_input("Press Enter to continue...")
+print("")
+print("Go make some bank!")
